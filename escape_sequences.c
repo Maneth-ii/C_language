09f2421ddb383caa618f5af4343fd0a28391ffac
Maneth-ii <manethbandara25@gmail.com>
2023-06-30 22:40:21 +0530

    initial

---
+++ escape_sequences.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    // escape sequences :
+    /*
+        \n = new Line
+        \t = tab space
+    */
+
+   printf("TOday\nis\nSunday\n");
+   printf("1\t2\t3\n4\t5\t6\n7\t8\t9");
+   return 0;
+}
--- helloWorld.c
+++ helloWorld.c
@@ -4,4 +4,9 @@ int main(){
     printf("Hello, world!\n");
     printf("I Love C");
     return 0;
+
+    //comment In 'C LANG';
+        /*multi line comment in 
+        
+        'C Lang'*/
 }
